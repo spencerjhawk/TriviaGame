9c87f1438d465ffbb8031b3b8a1e945dfc0b8026
Spencer Hawk <spencerjhawk@gmail.com>
2017-09-27 17:34:18 -0500

    added html css javascript

---
+++ app.js
@@ -0,0 +1,29 @@
+
+var correctAnswer = "Burlington, Iowa, New York Jets, 2017";
+var count = 45;
+var counter=setInterval(counter, 1000); //1000 will  run it every 1 second
+var rightAnswer = 0;
+var wrongAnswer = 0;
+
+function counter() {
+  count=count-1;
+  if (count <= 0)
+  {
+     alert("Game Over!");
+  }
+
+  //reset game
+  $("#counter").html(count);
+  reset();
+}
+
+//reset function
+function reset() {
+  if (count <= 0) {
+    count = 45
+  }
+  $("#counter").html(count);
+
+}
+
+$("#button1").click(function())
