3243b09b62640e22e3a3ac90fa973c564b63aef4
Spencer Hawk <spencerjhawk@gmail.com>
2017-09-27 18:12:11 -0500

    messing with app

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
-var correctAnswer = "Burlington, Iowa, New York Jets, 2017";
+var answers = [
+[true, false, false, false],
+[true, false, false, false],
+[false, false, true, false],
+];
+
 var count = 45;
 var counter=setInterval(counter, 1000); //1000 will  run it every 1 second
 var rightAnswer = 0;
@@ -25,5 +30,11 @@ function reset() {
   $("#counter").html(count);
 
 }
+
+function checkAnswer(row, column) {
+  console.log(
+  answers[row-1][column-1];
+);
+};
 
 
